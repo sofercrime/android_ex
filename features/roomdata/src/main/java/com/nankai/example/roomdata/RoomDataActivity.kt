3d349af4e8ef57cfba78760f5e4b8abd49d674e4
AlfonsoRoger <alfnsrgrs113@gmail.com>
2018-10-17 17:54:15 +0700

    hotfix set vector for imageView

--- features/roomdata/src/main/java/com/nankai/example/roomdata/RoomDataActivity.kt
+++ features/roomdata/src/main/java/com/nankai/example/roomdata/RoomDataActivity.kt
@@ -3,15 +3,54 @@ package com.nankai.example.roomdata
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.LifecycleRegistry
 import com.nankai.example.roomdata.databinding.ActivityRoomDataBinding
 import com.nankai.example.utilities.NavigationUtil
 import kotlinx.android.synthetic.main.activity_room_data.*
 
-class RoomDataActivity : AppCompatActivity() {
+class RoomDataActivity : AppCompatActivity() , LifecycleOwner {
+    private lateinit var mLifecycleRegistry : LifecycleRegistry
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val dataBinding: ActivityRoomDataBinding = DataBindingUtil.setContentView(this, R.layout.activity_room_data)
         content.setOnClickListener { _ -> NavigationUtil.goToMenus(this) }
+        mLifecycleRegistry = LifecycleRegistry(this)
+        mLifecycleRegistry.markState(Lifecycle.State.CREATED)
+//        handleDeepLink()
     }
+
+    override fun onStart() {
+        super.onStart()
+        mLifecycleRegistry.markState(Lifecycle.State.STARTED)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mLifecycleRegistry.markState(Lifecycle.State.RESUMED)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mLifecycleRegistry.markState(Lifecycle.State.DESTROYED)
+    }
+
+    override fun getLifecycle(): Lifecycle {
+        return mLifecycleRegistry
+    }
+
+//    private fun handleDeepLink() {
+//        if (intent.data != null) {
+//            val pathSegments = intent.data.pathSegments
+//            if (pathSegments.size > 1 && pathSegments[0].equals("cart", ignoreCase = true)) {
+//                val fragment = CartActivityFragment()
+//                val args = Bundle()
+//                args.putString(CartActivityFragment.KEY_CART_ID, pathSegments[1])
+//                fragment.setArguments(args)
+//                supportFragmentManager.beginTransaction().replace(R.id.content, fragment).commit()
+//            }
+//        }
+//    }
 }
