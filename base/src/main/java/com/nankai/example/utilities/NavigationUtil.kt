678413fa0fea5f86ab12c79a40d4799ec9bdff9f
AlfonsoRoger <alfnsrgrs113@gmail.com>
2018-10-18 11:00:46 +0700

    hotfix set vector for imageView

--- base/src/main/java/com/nankai/example/utilities/NavigationUtil.kt
+++ base/src/main/java/com/nankai/example/utilities/NavigationUtil.kt
@@ -3,7 +3,6 @@ package com.nankai.example.utilities
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
-import com.nankai.example.roomdata.RoomDataActivity
 import java.util.Locale
 
 object NavigationUtil {
@@ -20,8 +19,6 @@ object NavigationUtil {
 
     fun goToRoomData(context: Context){
         invokeDeepLink(context,String.format(Locale.US,"%s/roomdata", ROOT_ENDPOINT))
-        val intent = Intent(context, RoomDataActivity::class.java)
-        context.startActivity(intent)
     }
 
     fun goToNavigation(context: Context){
